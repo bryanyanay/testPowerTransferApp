04153f0f75805a42ea09ada6fa5f607ee4a181fc
Bryan Yan <74737261+bryanyanay@users.noreply.github.com>
2022-07-25 20:06:17 -0400

    Modify config

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 db_user = 'admin'
 db_password = 'qVX2yQTWDdoO6IzeKUX7RA39dVA_uZ'
-# i think we're supposed to use smth like this:
-# database = 'test_power_transfer_app?ssl=true&sslmode=verify-full&sslrootcert=C:\Users\\alber\OneDrive\Bryan\Code\\testPowerTransferApp\\root.crt'
-# which includes the root.crt; but it doesn't work for some reason and just the database name works, so we'll use that for now
 database = 'test_power_transfer_app' # [this also works, but maybe its not secure?? i'm not sure]
 db_host = 'northamerica-northeast2.d6301a4e-28af-45f8-a5ca-53fba3745b7a.gcp.ybdb.io' # i got this from the settings page for my cluster, in the connection parameters section
 db_port = 5433 # the port for YSQL i believe
+
+
+# i think we're supposed to use smth like this:
+# database = 'test_power_transfer_app?ssl=true&sslmode=verify-full&sslrootcert=C:\Users\\alber\OneDrive\Bryan\Code\\testPowerTransferApp\\root.crt'
+# which includes the root.crt; but it doesn't work for some reason and just the database name works, so we'll use that for now
