a5aef6868065d68924f07865f5065e49f3e3b251
Bryan Yan <74737261+bryanyanay@users.noreply.github.com>
2022-07-25 21:23:29 -0400

    Add methods

--- config.py
+++ config.py
@@ -8,3 +8,13 @@ db_port = 5433 # the port for YSQL i believe
 # i think we're supposed to use smth like this:
 # database = 'test_power_transfer_app?ssl=true&sslmode=verify-full&sslrootcert=C:\Users\\alber\OneDrive\Bryan\Code\\testPowerTransferApp\\root.crt'
 # which includes the root.crt; but it doesn't work for some reason and just the database name works, so we'll use that for now
+
+"""
+https://stackoverflow.com/questions/3327312/how-can-i-drop-all-the-tables-in-a-postgresql-database
+
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO public;
+COMMENT ON SCHEMA public IS 'standard public schema';
+"""
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import config as cfg
+from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, relationship, Session 
 from sqlalchemy import and_, select
 from sqlalchemy import Column, ForeignKey
 from sqlalchemy import Integer, String, DateTime, Numeric
 from sqlalchemy.sql import func
+
 
 engine = create_engine(
     f"postgresql://{cfg.db_user}:{cfg.db_password}@{cfg.db_host}:{cfg.db_port}/{cfg.database}",
@@ -21,6 +23,9 @@ class Vehicle(Base):
 
     users = relationship("User", back_populates="vehicles")
 
+    def __repr__(self):
+        return f'vid: {self.vid}, license: {self.license_plate}, owner: {self.uid}'
+
 class User(Base):
     __tablename__ = "users"
     uid = Column(Integer, primary_key=True)
@@ -29,15 +34,35 @@ class User(Base):
 
     vehicles = relationship("Vehicle", back_populates="users")
 
+    def __repr__(self):
+        return f'{self.first_name} {self.last_name}, uid: {self.uid}'
+
+    @classmethod
+    def getVehicles(cls, uid):
+        # returns a list of the user's vehicle objects 
+        with Session(engine) as s:
+            user = s.get(cls, uid)
+            if user is None:
+                raise ValueError(f'There is no user with id {uid}')
+            return list(user.vehicles) # after returning i believe the vehicle objects are "detached" and lose their connection with the corresponding row?? 
+    @classmethod
+    def getTransactions(cls, uid):
+        # returns a list of the user's Tx objects (both receiving and donating)
+        
+
+
 class Tx(Base):
     __tablename__ = "transactions"
     tid = Column(Integer, primary_key=True)
 
     start_time = Column(DateTime)
-    stop_time = Column(DateTime)
+    stop_time = Column(DateTime, server_default=func.now())
     recipient_id = Column(ForeignKey("vehicles.vid"))
     donor_id = Column(ForeignKey("vehicles.vid"))
     kwh_transferred = Column(Numeric)
+
+    def __repr__(self):
+        return f'{self.kwh_transferred} from vehicle {self.recipient_id} to vehicle {self.donor_id}; started at {self.start_time} and ended at {self.stop_time}'
 
 class TxIP(Base): # transactions in progress
     __tablename__ = "tx_in_progress"
@@ -48,38 +73,58 @@ class TxIP(Base): # transactions in progress
 
     
     @classmethod
-    def startTxIP(cls, recipient_id, donor_id):
-        with Session(engine) as s: # test if there is already a TxIP from donor to recipient
-            stmt = select(1).where(and_(TxIP.recipient_id == recipient_id, TxIP.donor_id == donor_id))
+    def start(cls, rid, did):
+        # returns the id of the txip created; this is used to stop the txip later
+        with Session(engine) as s: 
+            # test if there is already a TxIP from donor to recipient
+            stmt = select(1).where(and_(TxIP.recipient_id == rid, TxIP.donor_id == did))
             if s.execute(stmt).first() is not None:
-                raise ValueError(f'There is already a Tx in progress from vehicle {donor_id} to vehicle {recipient_id}')
-    @classmethod
-    def stopTxIP(cls):
-        with Session(engine) as s:
+                raise ValueError(f'There is already a Tx in progress from vehicle {did} to vehicle {rid}')
             
+            newTxIP = cls(recipient_id=rid, donor_id=did)
+            s.add(newTxIP)
+            s.commit()
+            return newTxIP.txip_id
+    @classmethod
+    def stop(cls, id):
+        with Session(engine) as s:
+            txip = s.get(cls, id)
+            if txip is None:
+                raise ValueError(f'There is no Tx currently in progress with id {id}')
+            
+            # can't use python's datetime.now(), i realized that datetime.now() is out of sync with yugabyte's now() [yugabyte is ahead by 4 hours]
+            newTx = Tx(start_time=txip.time, recipient_id=txip.recipient_id, donor_id=txip.donor_id)
+            s.add(newTx)
+            s.flush() # we need to flush so that newTx gets it's stop_time before we computer kwh_transferred
+            # right now the kwh_transferred is just the number of seconds that have passed; this should be replaced in the future
+            newTx.kwh_transferred = (newTx.stop_time - txip.time).total_seconds() 
+            
+            s.delete(txip)
+            s.commit()
 
 # Base.metadata.create_all(engine)
 
-TxIP.startTxIP(101, 102)
+for v in User.getVehicles(1):
+    print(v)
+
+def createTestObjects():
+    with Session(engine) as s:
+        u1 = User(first_name="Joe", last_name="Bob")
+        u2 = User(first_name="Alice", last_name="Allie")
+
+        v1 = Vehicle(license_plate="JJJBBB")
+        v2 = Vehicle(license_plate="BBBJJJ")
+        v3 = Vehicle(license_plate="AAAAAA")
+
+        u1.vehicles.append(v1)
+        u1.vehicles.append(v2)
+        u2.vehicles.append(v3)
+
+        s.add(u1)
+        s.add(u2)
+        s.commit()
 
 
-"""
-with Session(engine) as s:
-    u1 = User(first_name="Joe", last_name="Bob")
-    u2 = User(first_name="Alice", last_name="Allie")
-
-    v1 = Vehicle(license_plate="JJJBBB")
-    v2 = Vehicle(license_plate="BBBJJJ")
-    v3 = Vehicle(license_plate="AAAAAA")
-
-    u1.vehicles.append(v1)
-    u1.vehicles.append(v2)
-    u2.vehicles.append(v3)
-
-    s.add(u1)
-    s.add(u2)
-    s.commit()
-"""
 
 
 
